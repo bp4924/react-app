31316d0f4526a56d71cfc9f5156c6f8ede99be68
Bernie Petters <bp4924@gmail.com>
2023-03-25 13:41:38 -0400

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,29 @@
 function ListGroup() {
+  let items = [
+    "New York",
+    "London",
+    "Paris",
+    "Munich",
+    "Everybody talkin about Pop Muzik",
+  ];
+
+  items = ["New York", "London"];
+
+  const numberOfItemsMessage =
+    items.length < 5 ? <p>Only {items.length} items found</p> : null; //       {/* conditional constant using ternary operator, rendered in the return statement */}
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      {/* use <> for the fragment tag in the return.This tells react to use a Fragmant to wrap all the children*/}
+      <h1>List of Cities</h1>
+      {numberOfItemsMessage}
+      <ul className="list-group">
+        {/* Only html elements can be returned. use curly braces to wrap js commands */}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
