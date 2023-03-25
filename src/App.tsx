b4226bd5c9de7436079c38069d25bfe0a0dc107f
Bernie Petters <bp4924@gmail.com>
2023-03-25 11:59:00 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <Message />
+    </div>
+  ); // using self-closing syntax
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+// PascalCasing for function component
+function Message() {
+  return <h1>Hello World</h1>; // JSX
+}
+
+export default Message;
