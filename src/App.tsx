4b91509cd2c2bb205267071e0dfaca47f51c8ccc
Bernie Petters <bp4924@gmail.com>
2023-03-28 14:30:30 -0400

    dismissability

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
@@ -7,10 +8,15 @@ function App() {
 
   let heading = "Everybody talkin about";
   const text = "Warning!! Warning!!";
+  const text2 = "I see you";
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [alertVisible2, setAlertVisible2] = useState(false);
+
   return (
     <div>
       <ListGroup
@@ -18,15 +24,27 @@ function App() {
         heading={heading}
         onSelectItem={handleSelectItem}
       />
-      <Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>
+          {text2} <span className="fw-bolder fst-italic">Dynamic alert</span>{" "}
+          {text2} ...
+        </Alert>
+      )}
+      <Button
+        color="primary"
+        onClick={() => {
+          setAlertVisible(true);
+        }}
+      >
+        Click Me
+      </Button>
+
+      <Alert onClose={() => setAlertVisible(false)}>
         {text} <span className="fw-bolder fst-italic">This sort of works</span>{" "}
         {text}
       </Alert>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
-        Click Me
-      </Button>
     </div>
-  ); // using self-closing syntax
+  );
 }
 
 export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,12 +3,21 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode; // children of type ReactNode allows html elements (like span) to be passed in
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-warning" role="alert">
+    <div className="alert alert-warning alert-dismissible" role="alert">
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+      ',
     </div>
   );
 };
